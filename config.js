b8e3a38c2d3bd230d938594f568696ccceef4934
sameerpatel1234 <72080577+sameerpatel1234@users.noreply.github.com>
2021-12-16 21:01:43 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBQAv0RGH-nVlPAmFRr2flhOf9WcgK3pQ",
+  authDomain: "barter-system-3aa5b.firebaseapp.com",
+  databaseURL: "https://barter-system-3aa5b-default-rtdb.firebaseio.com",
+  projectId: "barter-system-3aa5b",
+  storageBucket: "barter-system-3aa5b.appspot.com",
+  messagingSenderId: "464762477441",
+  appId: "1:464762477441:web:9ab269016a11c19180e680"
+};
